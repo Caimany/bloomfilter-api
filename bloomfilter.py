4eec9a318cd970c01672b7da3de8d067bf28c7ad
caimujia <caimujia@gmail.com>
2015-06-15 20:28:41 +0800

    Update bloomfilter.py

--- bloomfilter.py
+++ bloomfilter.py
@@ -1,4 +1,4 @@
-import web , os
+import web , os ,json
 from pybloom import BloomFilter
 import cPickle as p
 #crontab   30 21 * * * curl 127.0.0.1/save && curl 127.0.0.1/find?id=1@#@$@#$#$#%%
@@ -6,11 +6,11 @@ import cPickle as p
 bloomplefile = 'bloom.ple'
 urls = (
     '/save', 'Save' ,
-    '/find', 'SomePage' ,
+    '/bloom', 'Bloom' ,
     '/init', 'Initfile'
 )
 
-exact=100000000
+exact=10000000
 #1000000000 = 4G mem
 #100000000 = 1.8G mem
 #10000000 = 300M men
@@ -22,7 +22,7 @@ if os.path.isfile(bloomplefile):
         print ("file is empty!")
         bloomfilter = BloomFilter(capacity=exact, error_rate=1/float(exact))
     else:
-        print ("file no empty!")
+        print ("file no empty , wait for loading.....")
         f = file(bloomplefile)
         bloomfilter = p.load(f)
         f.close()
@@ -31,14 +31,16 @@ else:
     print ("new file !")
     bloomfilter = BloomFilter(capacity=exact, error_rate=1/float(exact))
 
-class SomePage:
+class Bloom:
     def GET(self):
         user_data = web.input()
         #bloom_result=user_data.id
         bloom_result= bloomfilter.add(user_data.id)
         # False is first data  , True is duplicate data!
 
-        return str(bloom_result)
+        # return "<h1>" + str(bloom_result) + "</h1>"
+        return_info = [{'boolean' : bloom_result}]
+        return json.dumps(return_info)
 
 class Save:
     def GET(self):
